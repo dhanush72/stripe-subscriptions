3687ebfc3b6915891097c393fc63aa41e86d80c5
Dhanush R <dhanuraj7258@gmail.com>
2024-07-12 18:21:08 +0530

    :sparkles: Added kinde auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/providers/ThemeProvider';
 import { Navbar } from '@/components/Navbar';
+import TanStackProvider from '@/providers/TanstackProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -25,8 +26,10 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <Navbar />
-          {children}
+          <TanStackProvider>
+            <Navbar />
+            {children}
+          </TanStackProvider>
         </ThemeProvider>
       </body>
     </html>
